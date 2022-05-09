e2b7da631687e1aee6205d7009cf1f2d48211ab3
lixiaobing <lixiaobing@kuaishou.com>
2022-05-09 14:28:07 +0800

    添加cgroup

--- mydocker/container/init.go
+++ mydocker/container/init.go
@@ -11,14 +11,14 @@ func RunContainerInitProcess(command string, args []string) error {
 	log.Infof("command %s, args %s", command, args)
 
 	// private 方式挂载，不影响宿主机的挂载
-	err := syscall.Mount("", "/", "", syscall.MS_PRIVATE|syscall.MS_REC, "")
-	if err != nil {
-		log.Errorf("private 方式挂载 failed: %v", err)
-		return err
-	}
+	//err := syscall.Mount("", "/", "", syscall.MS_PRIVATE|syscall.MS_REC, "")
+	//if err != nil {
+	//	log.Errorf("private 方式挂载 failed: %v", err)
+	//	return err
+	//}
 
 	defaultMountFlags := syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV
-	err = syscall.Mount("proc", "/proc", "proc", uintptr(defaultMountFlags), "")
+	err := syscall.Mount("proc", "/proc", "proc", uintptr(defaultMountFlags), "")
 	if err != nil {
 		return err
 	}
