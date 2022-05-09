588dffa11a6df5a5fefe4a41a5fced82e79eb1c6
lixiaobing <lixiaobing@kuaishou.com>
2022-05-09 16:23:12 +0800

    管道添加

--- mydocker/container/init.go
+++ mydocker/container/init.go
@@ -1,26 +1,19 @@
 package container
 
 import (
+	"fmt"
 	log "github.com/sirupsen/logrus"
 	"io/ioutil"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 	"syscall"
 )
 
 func RunContainerInitProcess() error {
 
-	// private 方式挂载，不影响宿主机的挂载
-	err := syscall.Mount("", "/", "", syscall.MS_PRIVATE|syscall.MS_REC, "")
-	if err != nil {
-		log.Errorf("private 方式挂载 failed: %v", err)
-		return err
-	}
-
-	defaultMountFlags := syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV
-	err = syscall.Mount("proc", "/proc", "proc", uintptr(defaultMountFlags), "")
-	if err != nil {
+	if err := setUpMount(); err != nil {
 		return err
 	}
 
@@ -47,4 +40,60 @@ func readUserCommand() []string {
 	}
 	return strings.Split(string(msg), " ")
 
+}
+
+func setUpMount() error {
+	// 首先设置根目录为私有模式，防止影响pivot_root
+	if err := syscall.Mount("/", "/", "", syscall.MS_REC|syscall.MS_PRIVATE, ""); err != nil {
+		return fmt.Errorf("setUpMount Mount proc err: %v", err)
+	}
+	pwd, err := os.Getwd()
+	if err != nil {
+		return fmt.Errorf("get current location err: %v", err)
+	}
+	log.Infof("current location: %s", pwd)
+
+	err = pivotRoot(pwd)
+	if err != nil {
+		return err
+	}
+
+	// mount proc
+	defaultMountFlags := syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV
+	err = syscall.Mount("proc", "/proc", "proc", uintptr(defaultMountFlags), "")
+	if err != nil {
+		log.Errorf("proc挂载 failed: %v", err)
+		return err
+	}
+	syscall.Mount("tmpfs", "/dev", "tempfs", syscall.MS_NOSUID|syscall.MS_STRICTATIME, "mode=755")
+	return nil
+}
+
+func pivotRoot(root string) error {
+	if err := syscall.Mount(root, root, "bind", syscall.MS_BIND|syscall.MS_REC, ""); err != nil {
+		return fmt.Errorf("mount rootfs to itself error: %v", err)
+	}
+	pivotDir := filepath.Join(root, ".pivot_root")
+	if _, err := os.Stat(pivotDir); err == nil {
+		// 存在则删除
+		if err := os.Remove(pivotDir); err != nil {
+			return err
+		}
+	}
+	if err := os.Mkdir(pivotDir, 0777); err != nil {
+		return fmt.Errorf("mkdir of pivot_root err: %v", err)
+	}
+	// pivot_root 到新的rootfs，老的old_root现在挂载在rootfs/.pivot_root上
+	// 挂载点目前依然可以在mount命令中看到
+	if err := syscall.PivotRoot(root, pivotDir); err != nil {
+		return fmt.Errorf("pivot_root err: %v", err)
+	}
+	if err := syscall.Chdir("/"); err != nil {
+		return fmt.Errorf("chdir root err: %v", err)
+	}
+	pivotDir = filepath.Join("/", ".pivot_root")
+	if err := syscall.Unmount(pivotDir, syscall.MNT_DETACH); err != nil {
+		return fmt.Errorf("unmount pivot_root dir err: %v", err)
+	}
+	return os.Remove(pivotDir)
 }
